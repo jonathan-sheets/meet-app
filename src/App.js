cb1a5e948f125cd19f4f399d6d45f466ece62d63
jonathan-sheets <70708744+jonathan-sheets@users.noreply.github.com>
2021-01-05 08:54:17 -0700

    Change updateEvents function
    Updated function to display correct number of events by default - 24.

--- src/App.js
+++ src/App.js
@@ -14,34 +14,34 @@ class App extends Component {
     currentLocation: 'all',
   };
 
-  updateEvents = (location, eventCount) => {
-    const { currentLocation, numberOfEvents } = this.state;
-    if (location) {
-      getEvents().then((events) => {
-        const locationEvents =
-          location === 'all'
-          ? events
-          : events.filter((event) => event.location === location);
-        const filteredEvents = locationEvents.slice(0, numberOfEvents);
-        this.setState({
-          events: filteredEvents,
-          currentLocation: location,
-        });
-      });
-    } else {
-      getEvents().then((events) => {
-        const locationEvents =
-          currentLocation === 'all'
-          ? events
-          : events.filter((event) => event.location === currentLocation);
-        const filteredEvents = locationEvents.slice(0, eventCount);
-        this.setState({
-          events: filteredEvents,
-          numberOfEvents: eventCount,
-        });
-      });
-    }
-  };
+  // updateEvents = (location, eventCount) => {
+  //   const { currentLocation, numberOfEvents } = this.state;
+  //   if (location) {
+  //     getEvents().then((events) => {
+  //       const locationEvents =
+  //         location === 'all'
+  //         ? events
+  //         : events.filter((event) => event.location === location);
+  //       const filteredEvents = locationEvents.slice(0, numberOfEvents);
+  //       this.setState({
+  //         events: filteredEvents,
+  //         currentLocation: location,
+  //       });
+  //     });
+  //   } else {
+  //     getEvents().then((events) => {
+  //       const locationEvents =
+  //         currentLocation === 'all'
+  //         ? events
+  //         : events.filter((event) => event.location === currentLocation);
+  //       const filteredEvents = locationEvents.slice(0, eventCount);
+  //       this.setState({
+  //         events: filteredEvents,
+  //         numberOfEvents: eventCount,
+  //       });
+  //     });
+  //   }
+  // };
 
   componentDidMount() {
     this.mounted = true;
@@ -57,6 +57,33 @@ class App extends Component {
 
   componentWillUnmount(){
     this.mounted = false;
+  }
+
+  updateEvents = (location, eventCount) => {
+    if ( eventCount === undefined || 0 ){
+      this.setState({ eventCount: 24 });
+      getEvents().then((events) => {
+        const locationEvents = (location === 'all') ?
+          events :
+          events.filter((event) => event.location === location);
+        this.setState({
+          events: locationEvents.slice(0, this.state.eventCount)
+        });
+      });
+    }
+
+    if ( location === undefined ){
+      location = 'all';
+      this.setState({ eventCount });
+      getEvents().then((events) => {
+        const locationEvents = (location === 'all') ?
+          events :
+          events.filter((event) => event.location === location);
+        this.setState({
+          events: locationEvents.slice(0, this.state.eventCount)
+        });
+      });
+    }
   }
 
   render() {
