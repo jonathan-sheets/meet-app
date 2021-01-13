ea3631194a0eb371fcb5d8779ad940cd18e315aa
jonathan-sheets <70708744+jonathan-sheets@users.noreply.github.com>
2021-01-13 08:04:58 -0700

    updated end to end test to include correct class name for expanded event details

--- src/__tests__/EndToEnd.test.js
+++ src/__tests__/EndToEnd.test.js
@@ -21,19 +21,19 @@ describe('show/hide an event details', () => {
   });
 
   test('An event element is collapsed by default', async() => {
-    const eventDetails = await page.$('.event .details');
+    const eventDetails = await page.$('.event .expanded-details');
     expect(eventDetails).toBeNull();
   });
 
   test('User can expand an event to see its details', async () => {
     await page.click('.event .details-button');
-    const eventDetails = await page.$('.event .details');
+    const eventDetails = await page.$('.event .expanded-details');
     expect(eventDetails).toBeDefined();
   });
 
   test('User can collapse an event to hide its details', async () => {
     await page.click('.event .details-button');
-    const eventDetails = await page.$('.event .details');
+    const eventDetails = await page.$('.event .expanded-details');
     expect(eventDetails).toBeNull();
   });
 });
